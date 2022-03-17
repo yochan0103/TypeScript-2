da598a23b6309cd2c647dcb284525d658f621ebb
Yoshihiro Netsu <68014172+yochan0103@users.noreply.github.com>
2022-03-17 13:55:17 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import "./styles.css";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
 import { Text } from "../src/Text";
+import { UserProfile } from "../src/UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "よしお"
+  // hobbies: ["ゲーム", "ドラマ"]
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -16,6 +23,7 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="16px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todos) => (
